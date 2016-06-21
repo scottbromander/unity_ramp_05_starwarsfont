3af9361e1d651d34dee0854ad77f9a2227ab4452
Scott Bromander <scott@primeacademy.io>
2016-06-21 15:48:38 -0500

    initial commit

---
+++ Assets/ScrollZ.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollZ : MonoBehaviour {
+	public float scrollSpeed = 20;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		Vector3 localVectorUp = transform.TransformDirection (0, 1, 0);
+		pos += localVectorUp * scrollSpeed * Time.deltaTime;
+		transform.position = pos;
+	}
+}
